1c2633313bbd91cca896f6e745c349199dd13a40
iamish08 <ashish080397@gmail.com>
2018-10-29 21:45:04 +0530

    sql

--- atc.sql
+++ atc.sql
@@ -1,52 +1,51 @@
 create table atc(
-controller_id varchar(4) not null,
+controller_id integer(4) not null,
 password varchar(20),
 primary key(controller_id));
 
 create table pilot(
-pilot_id varchar(4) not null,
+pilot_id integer(4) not null,
 name varchar(20),
 password varchar(20),
 gender varchar(1),
 airlines varchar(20),
-controller_id varchar(4),
+controller_id integer(4),
 primary key(pilot_id),
 foreign key(controller_id) references atc(controller_id) on delete cascade);
 
 create table flight(
-flight_id varchar(4) not null,
+flight_id varchar(10) not null,
 aircraft_name varchar(10),
-priority varchar(10),
-no_of_pass varchar(3),
-emergency varchar(10),
+priority integer(2),
+no_of_pass integer(3),
+emergency varchar(3),
 airlines varchar(20),
 status varchar(20),
-type varchar(15),
+type varchar(20),
 source varchar(20),
 destination varchar(20),
 arrival varchar(20),
-pilot_id varchar(4),
+pilot_id integer(4),
 primary key(flight_id,destination,arrival),
 foreign key(pilot_id) references pilot(pilot_id) on delete cascade);
 
 create table passenger(
-p_id varchar(4) not null,
+p_id integer(4) not null,
 p_name varchar(20),
-password varchar(20),
 gender varchar(1),
-flight_id varchar(4),
+flight_id varchar(10),
 primary key(p_id),
 foreign key(flight_id) references flight(flight_id) on delete cascade);
 
 create table airport(
 airport_name varchar(20),
 distance varchar(5),
-time varchar(5),
+time time(5),
 primary key(airport_name));
 
 create table runway(
-runway_no varchar(2),
-controller_id varchar(4),
+runway_no integer(2),
+controller_id integer(4),
 is_available varchar(5),
 airport_name varchar(20),
 primary key(runway_no,controller_id),
@@ -55,9 +54,9 @@ foreign key(airport_name) references airport(airport_name) on delete cascade);
 
 create table weather(
 cur_date date,
-controller_id varchar(4),
-temperature varchar(3),
-wind_speed varchar(3),
+controller_id integer(4),
+temperature integer(2),
+wind_speed integer(3),
 wind_direction varchar(15),
 primary key(cur_date,controller_id),
 foreign key(controller_id) references atc(controller_id) on delete cascade); 
