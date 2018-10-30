d56c450a7eca8b3c8f9bd6ba7267b40f7549f9d8
iamish08 <ashish080397@gmail.com>
2018-10-30 23:57:56 +0530

    insert.sql new file added

---
+++ insert.sql
@@ -0,0 +1,16 @@
+insert into atc values(1001,'admin');
+
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2001,'Rajeev Bajpai','pwd2001@pilot','M','Indigo',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2002,'Trisha Mohan','pwd2002@pilot','F','Air Asia',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2003,'Karan Mehra','pwd2003@pilot','M','Vistara',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2004,'George Stalin','pwd2004@pilot','M','Emirates',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2005,'Kylian Mbappe','pwd2005@pilot','M','Air France',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2006,'Alex Morgan','pwd2006@pilot','F','Singapore Airlines',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2007,'Akash Anand','pwd2007@pilot','M','Air India',1001);
