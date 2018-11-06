ea58c36feda8cbce0f4e476a3e77601b795256d7
iamish08 <44507845+iamish08@users.noreply.github.com>
2018-11-06 21:27:46 +0530

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,313 @@
+insert into atc values(1001,'admin');
+
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2001,'Rajeev Bajpai','pwd2001@pilot','M','Indigo',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2002,'Trisha Mohan','pwd2002@pilot','F','Air Asia',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2003,'Karan Mehra','pwd2003@pilot','M','Vistara',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2004,'George Stalin','pwd2004@pilot','M','Emirates',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2005,'Kylian Mbappe','pwd2005@pilot','M','Air France',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2006,'Alex Morgan','pwd2006@pilot','F','Singapore Airlines',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2007,'Akash Anand','pwd2007@pilot','M','Air India',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2008,'Akshay Ravi','pwd2008@pilot','M','Air India',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2009,'Sarla Mehta','pwd2009@pilot','F','Air India',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2010,'Nitish Pathak','pwd2010@pilot','M','Air India',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2011,'Sargun Soni','pwd2011@pilot','F','Air India',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2012,'Jaypreet Singh','pwd2012@pilot','M','Air India',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2013,'Piyush Pandey','pwd2013@pilot','M','Air India',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2014,'Ayush Saxena','pwd2014@pilot','M','Air India',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2015,'Aryan Sharma','pwd2015@pilot','M','Indigo',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2016,'Akansha Sharma','pwd2016@pilot','F','Indigo',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2017,'Rashid Ali','pwd2017@pilot','M','Indigo',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2018,'Arun Rai','pwd2018@pilot','M','Indigo',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2019,'Somi Khan','pwd2019@pilot','F','Indigo',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2020,'Deepak Kumar','pwd2020@pilot','M','Indigo',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2021,'Shiv Singh','pwd2021@pilot','M','Indigo',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2022,'Urvi Parekh','pwd2022@pilot','F','Indigo',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2023,'Karan Kamra','pwd2023@pilot','M','Indigo',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2024,'Ashish Kumar','pwd2024@pilot','M','Air Asia',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2025,'Kovid Krishnan','pwd2025@pilot','M','Air Asia',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2026,'Harish Ahuja','pwd2026@pilot','M','Air Asia',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2027,'Neha Agnihotri','pwd2027@pilot','F','Air Asia',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2028,'Navin Richard','pwd2028@pilot','M','Air Asia',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2029,'Zakir Khan','pwd2029@pilot','M','Air Asia',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2030,'Mithali Joshi','pwd2030@pilot','F','Air Asia',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2031,'Rahim Qureshi','pwd2031@pilot','M','Vistara',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2032,'Sara Shinde','pwd2032@pilot','F','Vistara',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2033,'Vikas Sharma','pwd2033@pilot','M','Vistara',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2034,'Shikhar Chopra','pwd2034@pilot','M','Vistara',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2035,'Nikunj Pathak','pwd2035@pilot','M','Emirates',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2036,'Bryce Howard','pwd2036@pilot','F','Emirates',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2037,'Hugh Lloris','pwd2037@pilot','M','Air France',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2038,'Oliver Giroud','pwd2038@pilot','M','Air France',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2039,'Ryan Schure','pwd2039@pilot','M','Singapore Airlines',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2040,'Dean Holmes','pwd2040@pilot','M','Singapore Airlines',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2041,'Robin Barton','pwd2041@pilot','M','Singapore Airlines',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2042,'Ariana Jones','pwd2042@pilot','F','Singapore Airlines',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2043,'Mohan Kumar','pwd2043@pilot','M','Jet Airways',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2044,'Manoj Singh','pwd2044@pilot','M','Jet Airways',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2045,'Raima Sinha','pwd2045@pilot','F','Jet Airways',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2046,'Rohit Kaushal','pwd2046@pilot','M','Jet Airways',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2047,'Roshan Singh','pwd2047@pilot','M','Jet Airways',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2048,'Raman Singh','pwd2048@pilot','M','Spice Jet',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2049,'Pratik Parth','pwd2049@pilot','M','Spice Jet',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2050,'Arima Sen','pwd2050@pilot','F','Spice Jet',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2051,'Arunav Mishra','pwd2051@pilot','M','Spice Jet',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2052,'Vicky Grover','pwd2052@pilot','M','Spice Jet',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2053,'Kriti Sharma','pwd2053@pilot','F','Spice Jet',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2054,'Raj Soni','pwd2054@pilot','M','Spice Jet',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2055,'Ayushman Singh','pwd2055@pilot','M','Go Air',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2056,'Yami Sharma','pwd2056@pilot','F','Go Air',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2057,'Sidharth Sengar','pwd2057@pilot','M','Go Air',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2058,'Romil Singh','pwd2058@pilot','M','Go Air',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2059,'Anup Saini','pwd2059@pilot','M','Go Air',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2060,'Srishti Singh','pwd2060@pilot','F','Go Air',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2061,'Aviral Mishra','pwd2061@pilot','M','Go Air',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2062,'Wayne Alli','pwd2062@pilot','M','British Airways',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2063,'Harry Kane','pwd2063@pilot','M','British Airways',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2064,'Marcus Lingard','pwd2064@pilot','M','British Airways',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2065,'Sergio Garrado','pwd2065@pilot','M','Qatar Airways',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2066,'Toni Hummels','pwd2066@pilot','M','Lufthansa',1001);
+insert into pilot(pilot_id,name,password,gender,airlines,controller_id)
+values(2067,'Kevan TOrry','pwd2067@pilot','M','Etihad Airways',1001);
+
+
+
+insert into flight values('6E829','A320',1,50,'No','Indigo','On Time','Domestic','Bengaluru','Chennai','00:05:00','01:00:00',2001);
+insert into flight values('6E709','A320',1,50,'No','Indigo','On Time','Domestic','Cochin','Bengaluru','00:05:00','01:05:00',2015);
+insert into flight values('MH193','B738',1,84,'No','Singapore Airlines','On Time','International','Bengaluru','Kualampur','00:30:00','07:15:00',2006);
+
+
+insert into flight values('6E2135','A320',1,50,'No','Indigo','On Time','Domestic','Bengaluru','Delhi','00:40:00','03:00:00',2016);
+insert into flight values('9W236','A333',1,50,'No','Jet Airways','On Time','International','Bengaluru','Amsterdam','02:20:00','20:45:00',2043);
+insert into flight values('EY217','A332',1,80,'No','Ethiad Airways','On Time','International','Bengaluru','Abu Dhabi','04:25:00','07:05:00',2067);
+insert into flight values('I51582','A320',1,50,'No','Air Asia','On Time','Domestic','Bengaluru','Hyderabad','05:35:00','06:40:00',2002);
+
+insert into flight values('SG3301','DH8',1,50,'No','Spice Jet','On Time','Domestic','Chennai','Bengaluru','06:05:00','06:45:00',2048);
+insert into flight values('I51451','A20N',1,50,'No','Air Asia','On Time','Domestic','Bengaluru','Cochin','06:10:00','07:10:00',2024);
+
+insert into flight values('6E897','A320',1,50,'No','Indigo','On Time','Domestic','Mangalore','Bengaluru','07:30:00','08:20:00',2017);
+insert into flight values('AI339','A20N',1,80,'No','Air India','On Time','International','Bangkok','Bengaluru','05:45:00','08:25:00',2007);
+insert into flight values('EK564','B77W',1,50,'No','Emirates','On Time','International','Dubai','Bengaluru','03:50:00','08:50:00',2004);
+insert into flight values('I51567','A320',1,50,'No','Air Asia','On Time','Domestic','Bhubaneswar','Bengaluru','07:00:00','09:00:00',2025);
+insert into flight values('SG438','B747',1,50,'No','Spice Jet','On Time','Domestic','Vadodara','Bengaluru','07:30:00','09:20:00',2049);
+
+insert into flight values('I5162','A320',1,50,'No','Air Asia','On Time','Domestic','Bengaluru','Ranchi','08:32:00','10:50:00',2026);
+insert into flight values('G8272','A320',1,50,'No','Go Air','On Time','Domestic','Bengaluru','Patna','09:15:00','11:40:00',2055);
+insert into flight values('SG3465','DH8',1,50,'No','Spice Jet','On Time','Domestic','Bengaluru','Coimbatore','09:35:00','10:20:00',2048);
+insert into flight values('UK812','A320',1,50,'No','Vistara','On Time','Domestic','Bengaluru','Delhi','09:50:00','12:35:00',2003);
+insert into flight values('6E477','A320',1,50,'No','Indigo','On Time','Domestic','Bengaluru','Amritsar','10:00:00','13:10:00',2015);
+insert into flight values('9W141','A320',1,50,'No','Jet Airways','On Time','Domestic','Bengaluru','Patna','11:00:00','13:25:00',2044);
+
+insert into flight values('G8291','A320',1,50,'No','Go Air','On Time','Domestic','Hyderabad','Bengaluru','07:00:00','11:10:00',2056);
+insert into flight values('6E642','A320',1,50,'No','Indigo','On Time','Domestic','Jaipur','Bengaluru','09:00:00','11:30:00',2018);
+insert into flight values('SG767','A320 ',1,50,'No','Spice Jet','On Time','Domestic','Patna','Bengaluru','11:30:00','14:20:00',2050);
+insert into flight values('G8268','A320',1,50,'No','Go Air','On Time','Domestic','Goa','Bengaluru','14:00:00','15:10:00',2057);
+
+
+
+insert into flight values('IX468','B738',1,80,'No','Air India','On Time','International','Bengaluru','Singapore','12:35:00','19:55:00',2008);
+insert into flight values('AF181','B777',1,80,'No','Air France','On Time','International','Bengaluru','Paris','13:10:00','22:50:00',2037);
+insert into flight values('9W410','B38M',1,70,'No','Jet Airways','On Time','Domestic','Bengaluru','Mumbai','14:45:00','15:50:00',2045);
+
+insert into flight values('AI771','A320',1,50,'No','Air India','On Time','Domestic','Goa','Bengaluru','14:15:00','16:45:00',2009);
+insert into flight values('G8803','A320',1,50,'No','Go Air','On Time','Domestic','Port Blair','Bengaluru','15:15:00','17:55:00',2058);
+insert into flight values('SG3521','A320',1,50,'No','Spice Jet','On Time','Domestic','Kozikode','Bengaluru','17:20:00','18:25:00',2049);
+
+insert into flight values('6E361','A220N',1,80,'No','Indigo','On Time','Domestic','Bengaluru','Mumbai','15:20:00','17:20:00',2017);
+insert into flight values('I51728','A320',1,70,'No','Air Aisa','On Time','Domestic','Bengaluru','Jaipur','16:30:00','18:15:00',2025);
+insert into flight values('6E681','74H',1,120,'No','Lufthansa','On Time','International','Bengaluru','Frankfurt','17:00:00','23:10:00',2066);
+insert into flight values('EK567','B770',1,80,'No','Emirates','On Time','International','Bengaluru','Dubai','17:50:00','22:50:00',2035);
+
+
+
+
+insert into flight values('EY236','A321',1,50,'No','Ethiad Airways','On Time','International','Abu Dhabi','Bengaluru','14:15:00','19:30:00',2067);
+insert into flight values('QR8638','33X',1,50,'No','Qatar Airways','On Time','International','Doha','Bengaluru','13:10:00','19:45:00',2065);
+insert into flight values('9W660','B738',1,50,'No','Jet Airways','On Time','Domestic','Guwahati','Bengaluru','16:55:00','20:15:00',2046);
+insert into flight values('AI173','A320',1,70,'No','Air India','On Time','Domestic','Bengaluru','Delhi','21:10:00','23:55:00',2009);
+insert into flight values('G8426','74H',1,120,'No','Go Air','On Time','Domestic','Bengaluru','Mumbai','22:05:00','23:50:00',2057);
+
+insert into flight values('SQ502','B772',1,50,'No','Singapore Airlines','On Time','International','Singapore','Bengaluru','15:30:00','21:30:00',2039);
+insert into flight values('BA119','B777',1,80,'No','British Airways','On Time', 'International','London','Bengaluru','12:30:00','22:30:00',2062);
+insert into flight values('AF181','B777',1,80,'No','Air France','On Time','International','Paris','Bengaluru','13:10:00','22:50:00',2038);
+insert into flight values('I52469','A320',1,50,'No','Air Asia','On Time','Domestic','Indore','Bengaluru','22:05:00','23:55:00',2027);
+
+
+
+insert into flight values('6E681','A320',1,50,'No','Indigo','On Time','Domestic','Bengaluru','Pune','22:10:00','23:57:00',2019);
+
+
+insert into airport values
+('Bengaluru','0','00:00:00');
+
+insert into runway values
+(19,1001,'yes','Bengaluru');
+insert into runway values
+(53,1001,'yes','Bengaluru');
+
+
+insert into weather values
+('00:00:00','01:00:00',1001,20,9,'100° north');
+insert into weather values
+('01:00:00','02:00:00',1001,21,12,'90° north');
+insert into weather values
+('02:00:00','03:00:00',1001,21,15,'100° north');
+insert into weather values
+('03:00:00','04:00:00',1001,23,16,'100° north');
+insert into weather values
+('04:00:00','05:00:00',1001,24,20,'110° north');
+insert into weather values
+('05:00:00','06:00:00',1001,26,20,'110° north');
+insert into weather values
+('06:00:00','07:00:00',1001,26,20,'100° north');
+insert into weather values
+('07:00:00','08:00:00',1001,27,20,'110° north');
+
+
+insert into passenger values
+(3001,'Akash Singh','M','');
+insert into passenger values
+(3002,'Arun Sinha','M','');
+insert into passenger values
+(3003,'Babita Ganguly','F','');
+insert into passenger values
+(3004,'Soham Sharma','M','');
+insert into passenger values
+(3005,'Shubham Soni','M','');
+insert into passenger values
+(3006,'Shiv Kumar','M','');
+insert into passenger values
+(3007,'Ananya Gupta','F','');
+insert into passenger values
+(3008,'Sakshi Singh','F','');
+insert into passenger values
+(3009,'Shaan Mohd','M','');
+insert into passenger values
+(3010,'Rishabh Sengar','M','');
+insert into passenger values
+(3011,'Mohit Verma','M','');
+insert into passenger values
+(3012,'Harsh Sinha','M','');
+insert into passenger values
+(3013,'Anil Ahuja','M','');
+insert into passenger values
+(3014,'Shilpa Kumari','F','');
+insert into passenger values
+(3015,'Deepak Dubey','M','');
+insert into passenger values
+(3016,'Shahid Bhat','M','');
+insert into passenger values
+(3017,'Jyoti Singh','F','');
+insert into passenger values
+(3018,'Shivani Mishra','F','');
+insert into passenger values
+(3019,'Karan Shah','M','');
+insert into passenger values
+(3020,'Sambit Patra','M','');
+insert into passenger values
+(3021,'Akshat Jha','M','');
+insert into passenger values
+(3022,'Nikhil Advani','M','');
+insert into passenger values
+(3023,'Ravish Singh','M','');
+insert into passenger values
+(3024,'Aditya Kumar','M','');
+insert into passenger values
+(3025,'Diksha Kumari','F','');
+insert into passenger values
+(3026,'Ravi Shankar','M','');
+insert into passenger values
+(3027,'Alok Pal','M','');
+insert into passenger values
+(3028,'Asifa Shekh','F','');
+insert into passenger values
+(3029,'Shekhar Kapur','M','');
+insert into passenger values
+(3030,'Meera Gupta','F','');
+insert into passenger values
+(3031,'Ravi Prakash','M','');
+insert into passenger values
+(3032,'Saurabh Rajput','M','');
+insert into passenger values
+(3033,'Rohit Kumar','M','');
+insert into passenger values
+(3034,'Rohan Mishra','M','');
+insert into passenger values
+(3035,'Shruti Seth','F','');
+insert into passenger values
+(3036,'Arushi Singh','F','');
+insert into passenger values
+(3037,'Shagun Pandey','M','');
+insert into passenger values
+(3038,'Puneet Shah','M','');
+insert into passenger values
+(3039,'Gautam Singh','M','');
+insert into passenger values
+(3040,'Pritam Kumar','M','');
+
+
